e3910dc9cb8bcd4bacb4d44a18a846204dbc52c5
Khayal Farziyev <khayal.farziyev@pashabank.az>
2023-05-28 14:55:10 +0400

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "java-group-sip-1"
+
---
+++ src/main/java/az/iktlab/javasiplessons/lesson1/MyFirstClass.java
@@ -0,0 +1,64 @@
+package az.iktlab.javasiplessons.lesson1;
+
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+
+        // Bu kodun yazilma meqsedi "Hello world" sozunu console da chap etmekdir
+
+        /*
+         * Bu kodun yazilma meqsedi
+         * "Hello world"
+         * sozunu console da chap etmekdir
+         * */
+        System.out.println("Hello world");
+
+        byte a = 5;
+        byte b = 6;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        int c = 7;
+        int d = 8;
+        int e = c + d;
+
+        boolean isTrue = c > d;
+
+        System.out.println(isTrue);
+
+        int x = 9, y = 10, z = 2;
+
+        System.out.println(e);
+
+        System.out.println((z - x) * y);
+
+//        int f = 5, g = 6, aa = f + g;
+//        System.out.println(aa);
+
+//        byte f = 5, g = 6, aa = f + g;
+//        System.out.println(aa);
+
+
+        String ss = "Hello";
+        int bb = 5;
+
+        System.out.println(bb + ss);
+
+        boolean cc = true, dd = false;
+
+        System.out.println(cc);
+        System.out.println(dd);
+
+        /*
+         *  int a = 5, b= 6, c= a+b;
+         *  sout(c);
+         *
+         *  long a = 5, b= 6, c= a+b;
+         *  sout(c);
+         *
+         *  byte a = 5, b= 6, c= a+b;
+         *  sout(c);
+         * */
+    }
+}
